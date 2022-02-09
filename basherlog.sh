69824dfe854b450d797fcc04a419a99c75ae0807
Hugo Ávila <99145795+bioinformagica@users.noreply.github.com>
2022-02-09 00:55:28 -0300

    Add some docs

--- basherlog.sh
+++ basherlog.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+##########################
+# Returns the lenght of the longest
+# level string.
+# arg 1: One or more strings.
+# output: Echos the lenght of the longest string.
 function longest_string(){
   local max=0
 
@@ -11,6 +16,14 @@ function longest_string(){
 
 }
 
+##########################
+# Default log function
+# arg 1 (int): Variable to sign if a log massage
+#              will be printed (0) or not (1).
+# arg 2 (int): Lenght of padding.]
+# arg 2 (str): Log level ('ERROR', 'INFO', etc...)
+# arg * (str): Log message.
+# out:         None.
 function shlog(){
 
     local is_printed="${1}"
@@ -34,8 +47,13 @@ function shlog(){
     }      
 }
 
+##########################
+# Function to genarete level  
+# log functions (ex: shlog.error, shlog.info, ...).
+# arg 1 (str):  Log level: ERROR, WARNING, 'INFO'(default) or 'DEBUG'.
+# out        :  None
 function set_logger (){
-    local LOGGER_LEVEL="${LOGGER_LEVEL:-INFO}"
+    local logger_level="${1:-INFO}"
     local levels
     local is_printed
     local padding
@@ -51,14 +69,14 @@ function set_logger (){
 
     for level in "${!levels[@]}";do
         is_printed=1
-        [ "${levels[$level]}" -le "${levels[$LOGGER_LEVEL]}" ] && is_printed=0
+        [ "${levels[$level]}" -le "${levels[$logger_level]}" ] && is_printed=0
         eval "shlog.${level,,} (){ shlog ${is_printed} ${padding} ${level}  \${*}; }"
     done
 
 }
 
-function main (){
-    set_logger
+function test (){
+    set_logger "${@}"
     
     shlog.error "this is a test"
     shlog.warning "this is a test"
@@ -66,4 +84,4 @@ function main (){
     shlog.debug "this is a test"
 }
 
-[ "${BASH_SOURCE[0]}" == "${0}" ] && main "${@}"
+[ "${BASH_SOURCE[0]}" == "${0}" ] && test "${@}"
